d3afe9b4a58995968c2e1f32a634f511ae24587e
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-05-24 22:03:10 +0400

    Change(2. Shiritori eng): Added game_over

--- src/2.shiritoriEng.js
+++ src/2.shiritoriEng.js
@@ -1,5 +1,6 @@
 function Shiritory() {
   this.words = []
+  this.game_over = false
 }
 
 // Play word
@@ -7,15 +8,20 @@ Shiritory.prototype.play = function (anyWord) {
   const words = this.words,
     prevWord = words[words.length - 1]
 
-  if (words.length === 0) {
-    words.push(anyWord)
-  } else if (
-    words.indexOf(anyWord) === -1 &&
-    prevWord.charAt(prevWord.length - 1) === anyWord.charAt(0)
-  ) {
-    words.push(anyWord)
+  if (this.game_over) {
+    return 'This game is ended'
   } else {
-    return 'Game over!!'
+    if (words.length === 0) {
+      words.push(anyWord)
+    } else if (
+      words.indexOf(anyWord) === -1 &&
+      prevWord.charAt(prevWord.length - 1) === anyWord.charAt(0)
+    ) {
+      words.push(anyWord)
+    } else {
+      this.game_over = true
+      return 'Game over!!'
+    }
   }
 
   return words
@@ -23,6 +29,7 @@ Shiritory.prototype.play = function (anyWord) {
 // Restart game
 Shiritory.prototype.restart = function () {
   this.words.length = 0
+  this.game_over = false
   return 'Game restarted'
 }
 
@@ -39,4 +46,5 @@ console.log(myShiritori.words) // []
 // Words array should be set back to empty.
 console.log(myShiritori.play('hostess')) // ["hostess"]
 console.log(myShiritori.play('stash')) // ["hostess", "stash"]
+console.log(myShiritori.play('hostess')) // "game over"
 console.log(myShiritori.play('hostess')) // "game over"
